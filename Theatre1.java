dd89fdca3c5376175dec8b137c07b2dcdbb7971c
naviben <throttler67@gmail.com>
2022-10-11 20:19:46 +0530

    Theatre.java

---
+++ Theatre1.java
@@ -0,0 +1,13 @@
+class Theatre1
+{
+void bookTicket(int amount)
+{
+int balance= amount-120; 
+}
+public static void main(String[] args) 
+{
+Theatre1 show= new Theatre1();
+System.out.println("Balance is "+ 80); 
+}  
+}
+
