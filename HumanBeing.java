97c86e2b9ce9ffa0f01f3da948becc5341ef598b
naviben <throttler67@gmail.com>
2022-10-12 20:54:14 +0530

    HumanBeing.java

---
+++ HumanBeing.java
@@ -0,0 +1,17 @@
+class HumanBeing
+{
+int amount = 100000; 
+public static void main(String[] args)
+{
+HumanBeing hb = new HumanBeing(); 
+System.out.println(hb.amount); 
+hb.trip(20000); 
+}
+void trip(int amount)
+{
+System.out.println(amount);
+
+}
+}
+
+
