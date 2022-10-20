4f074acc773612445d67c91afa06be453b0a9cc3
naviben <throttler67@gmail.com>
2022-10-20 22:51:08 +0530

    ChennaiSilks.java

---
+++ ChennaiSilks.java
@@ -0,0 +1,14 @@
+class ChennaiSilks
+{
+ChennaiSilks()
+{
+//this(); 
+System.out.println("Buy 2 Get 1"); 
+}
+
+ChennaiSilks(int cc)
+{
+this(); 
+System.out.println("CC Purchase in Parent"); 
+}
+}
