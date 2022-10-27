5dc057c20bf362186453e9ed7a8ada0337f8cd0e
naviben <throttler67@gmail.com>
2022-10-27 18:55:51 +0530

    soundBar.java

---
+++ SoundBar.java
@@ -0,0 +1,17 @@
+abstract class SoundBar
+{
+SoundBar()
+{
+System.out.println("SoundBar Constructor "); 
+}
+public static void main(String[] args)
+{
+ 
+}
+abstract void bass(); 
+
+abstract void treble();
+
+abstract void vocals();
+
+}
